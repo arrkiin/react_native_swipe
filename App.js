4b2c9da4f1066d0aad5b5049d69553d9363eb340
arrkiin <arrkiin@gmail.com>
2018-02-20 16:04:15 +0100

    Working swipe with animation

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Deck from './src/Deck';
+import { Card, Button } from 'react-native-elements';
+
+const BORDER_RADIUS = 3;
 
 const DATA = [
     {
@@ -49,7 +52,41 @@ const DATA = [
 
 export default class App extends React.Component {
     renderCard = item => {
-        return <Text>{item.text}</Text>;
+        return (
+            <Card
+                key={item.id}
+                image={{ uri: item.uri }}
+                containerStyle={{
+                    elevation: 5,
+                    borderWidth: 0,
+                }}
+                wrapperStyle={{ padding: 0 }}
+            >
+                <View style={{ padding: 15 }}>
+                    <Text style={{ fontSize: 18, fontWeight: 'bold' }}>
+                        {item.text}
+                    </Text>
+                    <Text style={{ marginBottom: 10 }}>
+                        I can customize the Card further.
+                    </Text>
+                </View>
+                <View style={{ flexDirection: 'row', padding: 0 }}>
+                    <Button
+                        icon={{ name: 'code' }}
+                        backgroundColor="#03a9f4"
+                        title="View Now!"
+                        containerViewStyle={{
+                            flex: 1,
+                            marginLeft: 0,
+                            marginRight: 0,
+                        }}
+                        buttonStyle={{
+                            backgroundColor: 'grey',
+                        }}
+                    />
+                </View>
+            </Card>
+        );
     };
 
     render() {
