3b51658710b316278e4c90c01cfb86797a2cecc3
arrkiin <arrkiin@gmail.com>
2018-02-20 17:44:27 +0100

    Handle swipe direction and empty list

--- App.js
+++ App.js
@@ -56,43 +56,59 @@ export default class App extends React.Component {
             <Card
                 key={item.id}
                 image={{ uri: item.uri }}
-                containerStyle={{
-                    elevation: 5,
-                    borderWidth: 0,
-                }}
-                wrapperStyle={{ padding: 0 }}
+                containerStyle={styles.cardContainer}
+                wrapperStyle={styles.cardWrapper}
             >
-                <View style={{ padding: 15 }}>
-                    <Text style={{ fontSize: 18, fontWeight: 'bold' }}>
-                        {item.text}
-                    </Text>
+                <View style={styles.cardContent}>
+                    <Text style={styles.cardTitle}>{item.text}</Text>
                     <Text style={{ marginBottom: 10 }}>
                         I can customize the Card further.
                     </Text>
                 </View>
-                <View style={{ flexDirection: 'row', padding: 0 }}>
+                <View style={styles.cardButtonContainer}>
                     <Button
                         icon={{ name: 'code' }}
                         backgroundColor="#03a9f4"
                         title="View Now!"
-                        containerViewStyle={{
-                            flex: 1,
-                            marginLeft: 0,
-                            marginRight: 0,
-                        }}
-                        buttonStyle={{
-                            backgroundColor: 'grey',
-                        }}
+                        containerViewStyle={styles.buttonViewContainer}
+                        buttonStyle={styles.button}
                     />
                 </View>
             </Card>
         );
     };
-
+    renderNoMoreCards = () => {
+        return (
+            <Card
+                containerStyle={styles.cardContainer}
+                wrapperStyle={styles.cardWrapper}
+            >
+                <View style={styles.cardContent}>
+                    <Text style={styles.cardTitle}>All Done!</Text>
+                    <Text style={{ marginBottom: 10 }}>
+                        There's no more content here!
+                    </Text>
+                </View>
+                <View style={styles.cardButtonContainer}>
+                    <Button
+                        icon={{ name: 'code' }}
+                        backgroundColor="#03a9f4"
+                        title="Get more!"
+                        containerViewStyle={styles.buttonViewContainer}
+                        buttonStyle={styles.button}
+                    />
+                </View>
+            </Card>
+        );
+    };
     render() {
         return (
             <View style={styles.container}>
-                <Deck data={DATA} renderCard={this.renderCard} />
+                <Deck
+                    data={DATA}
+                    renderCard={this.renderCard}
+                    renderNoMoreCards={this.renderNoMoreCards}
+                />
             </View>
         );
     }
@@ -102,5 +118,32 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
+    },
+    cardContainer: {
+        elevation: 5,
+        borderWidth: 0,
+        padding: 0,
+    },
+    cardWrapper: {
+        padding: 0,
+    },
+    cardContent: {
+        padding: 15,
+    },
+    cardTitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    cardButtonContainer: {
+        flexDirection: 'row',
+        padding: 0,
+    },
+    buttonViewContainer: {
+        flex: 1,
+        marginLeft: 0,
+        marginRight: 0,
+    },
+    button: {
+        backgroundColor: 'grey',
     },
 });
