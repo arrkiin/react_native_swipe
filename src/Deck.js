7eb801498728e2f97f816bf380fbff01188fafed
arrkiin <arrkiin@gmail.com>
2018-02-21 09:40:42 +0100

    Reset deck index while receiving new data

--- src/Deck.js
+++ src/Deck.js
@@ -38,6 +38,11 @@ class Deck extends Component {
         });
         this.state = { panResponder, position, index: 0 };
     }
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.data !== this.props.data) {
+            this.setState({ index: 0 });
+        }
+    }
     componentWillUpdate() {
         UIManager.setLayoutAnimationEnabledExperimental &&
             UIManager.setLayoutAnimationEnabledExperimental(true);
